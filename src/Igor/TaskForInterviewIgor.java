e4780ebd3ed27cd0456864b691d7902683a8f1ad
Garik-create <pfdfkbybujhm@yandex.ru>
2023-05-05 21:37:26 +0300

    - добавил решение задания 26

--- src/Igor/TaskForInterviewIgor.java
+++ src/Igor/TaskForInterviewIgor.java
@@ -1,7 +1,7 @@
 package Igor;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TaskForInterviewIgor {
 
@@ -33,6 +33,7 @@ public class TaskForInterviewIgor {
         bar(m);
         System.out.print(m);
     }
+
     static void bar(String m) {
         m += " World!";
     }
@@ -44,10 +45,10 @@ public class TaskForInterviewIgor {
         System.out.println("Задание 22");
         String s1 = "abc";
         String s2 = "abc";
-        String s3= new String("abc");
-        System.out.println("s1 == s2 ? "+(s1==s2));
-        System.out.println("s1 == s3 ? "+(s1==s3));
-        System.out.println("s1 equals s3 ? "+(s1.equals(s3)));
+        String s3 = new String("abc");
+        System.out.println("s1 == s2 ? " + (s1 == s2));
+        System.out.println("s1 == s3 ? " + (s1 == s3));
+        System.out.println("s1 equals s3 ? " + (s1.equals(s3)));
     }
 
     //task23
@@ -56,12 +57,59 @@ public class TaskForInterviewIgor {
             System.out.println("This is abstract class constructor");
         }
     }
+
     static class OurDemoClass extends OurAbstractClass {
         public OurDemoClass() {
             System.out.println("This is demo class constructor");
         }
+
         public static void main(String[] args) {
             OurDemoClass ourDemoClass = new OurDemoClass();
+        }
+    }
+
+    public static void task26() {
+
+        System.out.println();
+        System.out.println("Задание 26");
+
+        //слова, которые ищем в строке
+        String first = "первый";
+        String second = "второй";
+        String third = "третий";
+        String forth = "четвёртый";
+
+        // хешмапа слов, которые ищем в строке
+        HashMap<String, Integer> wordsForFinding = new HashMap<>();
+        wordsForFinding.put(first, 0);
+        wordsForFinding.put(second, 0);
+        wordsForFinding.put(third, 0);
+        wordsForFinding.put(forth, 0);
+
+        //строка, в которой ищем нужные слова
+        String text = first + " " + second + " " + third + " " + first + " " + second + " " + first;
+        System.out.println("text = " + text);
+
+        //хэшмапа слов в строке, в которой ищем нужные слова
+        HashMap<Integer, String> inputWords = new HashMap<>();
+
+        String[] words = text.split(" ");//разделяем слова в строке
+        for (int i = 0; i < words.length; i++) {
+            inputWords.put(i, words[i]);//заполняем хэшмапу словами из строки
+        }
+        System.out.println("inputWords = " + inputWords);
+        System.out.println("wordsForFinding = " + wordsForFinding);
+
+        //считаем количество найденных слов в исходной строке и выводим на экран
+        for (Map.Entry<String, Integer> word : wordsForFinding.entrySet()) {
+            for (String value : inputWords.values()) {
+                if (word.getKey().equals(value)) {
+                    Integer count = word.getValue();
+                    count++;
+                    word.setValue(count);
+                }
+            }
+            System.out.println(word);
         }
     }
 }
